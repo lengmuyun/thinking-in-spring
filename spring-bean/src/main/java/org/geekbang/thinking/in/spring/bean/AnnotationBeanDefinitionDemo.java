ef93b19c93d49437ae28c6883d61fca56050202e
lengmuyun <971434648@qq.com>
2021-07-09 07:52:40 +0800

    Bean Definition注册

---
+++ spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/AnnotationBeanDefinitionDemo.java
@@ -0,0 +1,90 @@
+package org.geekbang.thinking.in.spring.bean;
+
+import org.geekbang.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.jws.soap.SOAPBinding;
+import java.util.Map;
+
+/**
+ * 注解BeanDefinition示例
+ *
+ * @author fangkuangzhang
+ * @date 2021/7/9 7:38
+ */
+// 3. 通过@Import来进行导入
+@Import(AnnotationBeanDefinitionDemo.Config.class)
+public class AnnotationBeanDefinitionDemo {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(AnnotationBeanDefinitionDemo.class);
+
+        // 注册命名bean
+        registerUserBeanDefinition(applicationContext, "fkz111-user");
+        // 注册非命名bean
+        registerUserBeanDefinition(applicationContext);
+
+        applicationContext.refresh();
+
+        Config config = applicationContext.getBean(Config.class);
+        System.out.println("config bean: " + config);
+
+        lookupCollectionByType(applicationContext);
+
+        applicationContext.close();
+    }
+
+    private static void lookupCollectionByType(BeanFactory beanFactory) {
+        if (beanFactory instanceof ListableBeanFactory) {
+            ListableBeanFactory lbf = (ListableBeanFactory) beanFactory;
+            Map<String, User> userMap = lbf.getBeansOfType(User.class);
+            System.out.println("查找到的所有的 User 集合对象：" + userMap);
+        }
+    }
+
+    private static void registerUserBeanDefinition(BeanDefinitionRegistry registry, String beanName) {
+        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(User.class);
+        beanDefinitionBuilder.addPropertyValue("id", 1L);
+        beanDefinitionBuilder.addPropertyValue("name", "fkz");
+
+        if (StringUtils.hasText(beanName)) {
+            registry.registerBeanDefinition(beanName, beanDefinitionBuilder.getBeanDefinition());
+        } else {
+            BeanDefinitionReaderUtils.registerWithGeneratedName(beanDefinitionBuilder.getBeanDefinition(), registry);
+        }
+    }
+
+    private static void registerUserBeanDefinition(BeanDefinitionRegistry registry) {
+        registerUserBeanDefinition(registry, null);
+    }
+
+    // 2.通过@Component方式
+    @Component
+    public static class Config {
+
+    }
+
+    // 1. 通过@Bean方式定义
+    /**
+     * 通过java注解的方式，定义了一个Bean
+     * @return
+     */
+    @Bean(name = {"user", "fkz-user"})
+    public User user() {
+        User user = new User();
+        user.setId(1L);
+        user.setName("fkz");
+        return user;
+    }
+
+}
