44daeb1c7b19efaa1aae5debe92c067c5888ab4b
lengmuyun <971434648@qq.com>
2021-07-14 21:24:47 +0800

    Bean初始化以及延时加载

---
+++ spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/BeanInitializationDemo.java
@@ -0,0 +1,39 @@
+package org.geekbang.thinking.in.spring.bean.definition;
+
+import org.geekbang.thinking.in.spring.bean.factory.DefaultUserFactory;
+import org.geekbang.thinking.in.spring.bean.factory.UserFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+/**
+ * Bean初始化示例
+ *
+ * @author fangkuangzhang
+ * @date 2021/7/14 20:38
+ */
+@Configuration
+public class BeanInitializationDemo {
+
+    public static void main(String[] args) {
+        // 创建 BeanFactory 容器
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        // 注册 Configuration Class(配置类)
+        applicationContext.register(BeanInitializationDemo.class);
+        // 启动 Spring 应用上下文
+        applicationContext.refresh();
+        System.out.println("Spring 应用上下文已启动...");
+        UserFactory userFactory = applicationContext.getBean(UserFactory.class);
+        System.out.println(userFactory);
+        // 关闭 Spring 应用上下文
+        applicationContext.close();
+    }
+
+    @Bean(initMethod = "initUserFactory")
+    @Lazy
+    public DefaultUserFactory userFactory() {
+        return new DefaultUserFactory();
+    }
+
+}
--- spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/factory/DefaultUserFactory.java
+++ spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/factory/DefaultUserFactory.java
@@ -1,10 +1,29 @@
 package org.geekbang.thinking.in.spring.bean.factory;
 
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+
 /**
  * 默认{@link UserFactory}实现
  *
  * @author fangkuangzhang
  * @date 2021/7/11 16:56
  */
-public class DefaultUserFactory implements UserFactory {
+public class DefaultUserFactory implements UserFactory, InitializingBean {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("@PostConstruct 初始化...");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("InitializingBean#afterPropertiesSet() 初始化...");
+    }
+
+    public void initUserFactory() {
+        System.out.println("initUserFactory 初始化...");
+    }
+
 }
