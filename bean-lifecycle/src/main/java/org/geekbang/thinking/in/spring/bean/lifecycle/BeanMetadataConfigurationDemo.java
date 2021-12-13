50ad59ab72263e7880f2122fba1b892b090e26e5
lengmuyun <971434648@qq.com>
2021-12-13 10:29:27 +0800

    注解BeanDefinition解析示例

--- bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/bean/lifecycle/BeanMetadataConfigurationDemo.java
+++ bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/bean/lifecycle/BeanMetadataConfigurationDemo.java
@@ -17,13 +17,16 @@ public class BeanMetadataConfigurationDemo {
 
     public static void main(String[] args) {
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        // 实例化基于 Properties 资源 BeanDefinitionReader
         PropertiesBeanDefinitionReader reader = new PropertiesBeanDefinitionReader(beanFactory);
-        String location = "classpath:/META-INF/user.properties";
-        Resource resource = new ClassPathResource("/META-INF/user.properties");
+        String location = "/META-INF/user.properties";
+        // 基于 ClassPath 加载 properties 资源
+        Resource resource = new ClassPathResource(location);
+        // 指定字符编码 UTF-8
         EncodedResource encodedResource = new EncodedResource(resource, "UTF-8");
         int beansNumber = reader.loadBeanDefinitions(encodedResource);
         System.out.println("beans number: " + beansNumber);
-
+        // 通过 Bean Id 和类型进行依赖查找
         User user = beanFactory.getBean("user", User.class);
         System.out.println("user: " + user);
     }
