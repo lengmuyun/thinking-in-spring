b4c7cf3f5043aab134e88b8700cea9aa545c63f2
lengmuyun <971434648@qq.com>
2022-01-06 22:06:02 +0800

    Aware接口回调

--- bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/bean/lifecycle/BeanInitializationLifecycleDemo.java
+++ bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/bean/lifecycle/BeanInitializationLifecycleDemo.java
@@ -3,9 +3,12 @@ package org.geekbang.thinking.in.spring.bean.lifecycle;
 import org.geekbang.thinking.in.spring.ioc.overview.domain.SuperUser;
 import org.geekbang.thinking.in.spring.ioc.overview.domain.User;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.PropertyValues;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Bean 初始化生命周期示例
@@ -16,6 +19,12 @@ import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 public class BeanInitializationLifecycleDemo {
 
     public static void main(String[] args) {
+        executeBeanFactory();
+        System.out.println("=====================分隔符=====================");
+        executeApplicationContext();
+    }
+
+    private static void executeBeanFactory() {
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
         beanFactory.addBeanPostProcessor(new MyInstantiationAwareBeanPostProcessor());
         XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
@@ -32,6 +41,26 @@ public class BeanInitializationLifecycleDemo {
         // 通过 Bean Id 和类型进行依赖查找
         UserHolder userHolder = beanFactory.getBean("userHolder", UserHolder.class);
         System.out.println("userHolder: " + userHolder);
+    }
+
+    private static void executeApplicationContext() {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext();
+        applicationContext.setConfigLocations("META-INF/dependency-lookup-context.xml", "META-INF/bean-constructor-dependency-injection.xml");
+        applicationContext.refresh();
+
+        // 通过 Bean Id 和类型进行依赖查找
+        User user = applicationContext.getBean("user", User.class);
+        System.out.println("user: " + user);
+
+        // 通过 Bean Id 和类型进行依赖查找
+        SuperUser superUser = applicationContext.getBean("superUser", SuperUser.class);
+        System.out.println("superUser: " + superUser);
+
+        // 通过 Bean Id 和类型进行依赖查找
+        UserHolder userHolder = applicationContext.getBean("userHolder", UserHolder.class);
+        System.out.println("userHolder: " + userHolder);
+
+        applicationContext.close();
     }
 
     public static class MyInstantiationAwareBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
@@ -56,6 +85,21 @@ public class BeanInitializationLifecycleDemo {
             }
             return true;
         }
+
+        @Override
+        public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName) throws BeansException {
+            if ("userHolder".equals(beanName) && UserHolder.class.equals(bean.getClass())) {
+                MutablePropertyValues mpvs = (pvs instanceof MutablePropertyValues) ? (MutablePropertyValues) pvs : new MutablePropertyValues();
+                mpvs.add("number", "1");
+                if (mpvs.contains("description")) {
+                    mpvs.removePropertyValue("description");
+                }
+                mpvs.add("description", "The user holder V2");
+                return mpvs;
+            }
+            return null;
+        }
+
     }
 
 }
--- bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/bean/lifecycle/UserHolder.java
+++ bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/bean/lifecycle/UserHolder.java
@@ -1,6 +1,13 @@
 package org.geekbang.thinking.in.spring.bean.lifecycle;
 
 import org.geekbang.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 
 /**
  * User Holder 类
@@ -8,9 +15,21 @@ import org.geekbang.thinking.in.spring.ioc.overview.domain.User;
  * @author fangkuangzhang
  * @date 2022/1/4 16:39
  */
-public class UserHolder {
+public class UserHolder implements BeanNameAware, BeanClassLoaderAware, BeanFactoryAware, EnvironmentAware {
 
     private User user;
+
+    private ClassLoader classLoader;
+
+    private BeanFactory beanFactory;
+
+    private String beanName;
+
+    private Integer number;
+
+    private String description;
+
+    private Environment environment;
 
     public UserHolder(User user) {
         this.user = user;
@@ -24,11 +43,50 @@ public class UserHolder {
         return user;
     }
 
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public String toString() {
         return "UserHolder{" +
                 "user=" + user +
+                ", beanName='" + beanName + '\'' +
+                ", number=" + number +
+                ", description='" + description + '\'' +
                 '}';
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        this.beanName = name;
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
     }
 
 }
