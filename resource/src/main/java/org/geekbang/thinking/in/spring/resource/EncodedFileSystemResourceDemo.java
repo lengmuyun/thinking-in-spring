d5166cabdba953eb3acc4b58a7e5cc5e6e406c9e
lengmuyun <971434648@qq.com>
2022-02-03 20:35:07 +0800

    带有字符编码的 {@link FileSystemResource} 示例

---
+++ resource/src/main/java/org/geekbang/thinking/in/spring/resource/EncodedFileSystemResourceDemo.java
@@ -0,0 +1,25 @@
+package org.geekbang.thinking.in.spring.resource;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.support.EncodedResource;
+
+import java.io.IOException;
+
+/**
+ * 带有字符编码的 {@link FileSystemResource} 示例
+ *
+ * @author fangkuangzhang
+ * @date 2022/2/3 20:20
+ * @see FileSystemResource
+ * @see EncodedResource
+ */
+public class EncodedFileSystemResourceDemo {
+
+    public static void main(String[] args) throws IOException {
+        String javaFilePath = System.getProperty("user.dir") + "/resource/src/main/java/org/geekbang/thinking/in/spring/resource/EncodedFileSystemResourceDemo.java";
+        FileSystemResource fileSystemResource = new FileSystemResource(javaFilePath);
+        EncodedResource encodedResource = new EncodedResource(fileSystemResource, "UTF-8");
+        System.out.println(IOUtils.toString(encodedResource.getInputStream(), "UTF-8"));
+    }
+}
